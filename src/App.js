9cb17c697c20f2a55348a9d787bcd903260f6b29
Andrew Holmes <acholmes001@gmail.com>
2022-10-06 21:01:34 -0400

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import './App.css';
-import { useEffect } from 'react'
+import {
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
+
+import LandingPage from './containers/LandingPage';
 
 function App() {
-  useEffect(() => {
-    const script = document.createElement('script');
-  
-    script.src = "https://cdn.jsdelivr.net/npm/p5@1.4.2/lib/p5.js";
-    script.async = true;
-
-    document.body.appendChild(script);
-  
-    return () => {
-      document.body.removeChild(script);
-    }
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <div> Initial commit </div>
-        <canvas></canvas>
-      </header>
-    </div>
+    <>
+      <LandingPage />
+      <div> refactor! </div> 
+    </>
   )
 }
 
---
+++ src/containers/LandingPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LandingPage = () => 
+{
+    return (
+        <div className="App">
+        <header className="App-header">
+            <div> Initial commit </div>
+            <canvas></canvas>
+        </header>
+    </div>
+    )
+}
+
+export default LandingPage

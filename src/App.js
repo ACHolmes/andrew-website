544995bc418bbea54db5e04c1fbdc9a9cd6afad2
Andrew Holmes <acholmes001@gmail.com>
2022-10-06 20:44:33 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+  
+    script.src = "https://cdn.jsdelivr.net/npm/p5@1.4.2/lib/p5.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div> Initial commit </div>
+        <canvas></canvas>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
